ea0236fae18927a64f17dc45d751447f32adac21
hikmetsekerc <hikmetsekerci0@gmail.com>
2023-08-04 09:42:26 -0400

    the code

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,10 @@ public class Main {
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Ctrl+F8.
             System.out.println("i = " + i);
+
+            for (int i = 1; i <= 5; i++) {
+
+      
         }
     }
 }
